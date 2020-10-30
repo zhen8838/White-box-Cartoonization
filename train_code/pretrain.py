7d6ffdb60a72c2d4d826c3ad869b8d91f4416791
郑启航 <597323109@qq.com>
2020-10-30 16:14:08 +0800

    fix smoething

--- train_code/pretrain.py
+++ train_code/pretrain.py
@@ -23,7 +23,7 @@ os.environ["CUDA_VISIBLE_DEVICES"]="0"
 def arg_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("--patch_size", default = 256, type = int)
-    parser.add_argument("--batch_size", default = 16, type = int)     
+    parser.add_argument("--batch_size", default = 4, type = int)     
     parser.add_argument("--total_iter", default = 50000, type = int)
     parser.add_argument("--adv_train_lr", default = 2e-4, type = float)
     parser.add_argument("--gpu_fraction", default = 0.5, type = float)
--- train_code/selective_search/structure.py
+++ train_code/selective_search/structure.py
@@ -2,7 +2,7 @@ import numpy as np
 from skimage.segmentation import find_boundaries
 from skimage.segmentation import felzenszwalb
 from scipy.ndimage import find_objects
-import measure
+from selective_search import measure
 
 
 class HierarchicalGrouping(object):
